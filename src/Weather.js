f1ffb89a1ca58aac7e2a490464901bf31db40ec4
Nadiia Kyshinska <2014vedina@gmail.com>
2023-03-25 22:30:39 -0600

    Add API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import { FaSistrix } from "react-icons/fa";
+import axios from "axios";
+import { InfinitySpin } from "react-loader-spinner";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="search-form">
-        <div className="row">
-          <div className="col-10">
-            <input
-              type="text"
-              className="form-control w-100 search-input no-outline"
-              placeholder="Search for location"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
+
+  function handleResponse(response) {
+    //Displaying the Weather Forecast Data
+    setWeatherData({
+      loaded: true,
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      description: response.data.weather[0].description,
+      iconUrl: "https://openweathermap.org/img/wn/10d@2x.png",
+    });
+  }
+
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        <form className="search-form">
+          <div className="row">
+            <div className="col-10">
+              <input
+                type="text"
+                className="form-control w-100 search-input no-outline"
+                placeholder="Search for location"
+              />
+            </div>
+            <div className="col-2">
+              <button type="submit" className="btn">
+                {" "}
+                <FaSistrix className="search-icon" />
+              </button>
+            </div>{" "}
           </div>
-          <div className="col-2">
-            <button type="submit" className="btn">
-              {" "}
-              <FaSistrix className="search-icon" />
-            </button>
-          </div>{" "}
+        </form>
+        <h1>{weatherData.city}</h1>
+        <p>Friday, 10:50</p>
+        <p className="text-capitalize">{weatherData.description}</p>
+        <div className="row mt-4">
+          <div className="col-6 d-flex">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="weather-icon"
+            />
+            <span className="temperature">{weatherData.temperature}</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
+            <p>Humidity: {weatherData.humidity}%</p>
+            <p>Wind: {weatherData.wind} km/h</p>
+          </div>
         </div>
-      </form>
-      <h1>Mykolaiv</h1>
-      <p>Friday, 10:50</p>
-      <p>Sunny</p>
-      <div className="row mt-4">
-        <div className="col-6 d-flex">
-          <img
-            src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v3/Condition_Card/SunnyDayV3.svg"
-            alt="weather-icon"
-          />
-          <span className="temperature">6</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <p>Humidity: 93%</p>
-          <p>Wind: 12 km/h</p>
-        </div>
+        <footer>
+          This project was coded by Nadiia Kyshinska and is{" "}
+          <a href="https://github.com/Vedina2022/weather-forecast-react-2">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
-      <footer>
-        This project was coded by Nadiia Kyshinska and is{" "}
-        <a href="https://github.com/Vedina2022/weather-forecast-react-2">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    //OpenWeather API call
+    const apiKey = "4a3d84f7f5a3938304bb369a18a3daff";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return <InfinitySpin width="500" color="white" />;
+  }
 }
